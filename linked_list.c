4d9a7897fb4d997311f105a543d087b3857ac8a5
bliutwo <bliutwo@cs.stanford.edu>
2021-06-09 23:19:11 -0700

    Create hello world

---
+++ linked_list.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+typedef struct list {
+    item_type item;     // data item
+    struct list *next;  // point to successor
+} list;
+*/
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
